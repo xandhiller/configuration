5544cbcb1c48d90559eefb8fc335fdb58d10d0a0
Alex Hiller <xandhiller@gmail.com>
2018-10-14 00:11:45 +1100

    Added .vim/ directory copy to pullconfigs.sh

--- pullconfigs.sh
+++ pullconfigs.sh
@@ -3,9 +3,11 @@ bashrc="/home/polluticorn/.bashrc"
 ith="/home/polluticorn/.config/i3/"
 ithb="/home/polluticorn/.config/i3blocks/"
 xdef="/home/polluticorn/.Xdefaults"
+vimplugins="/home/polluticorn/.vim/"
 
-cp    $vimrc   .
-cp    $bashrc  .
-cp -R $ith     .
-cp -R $ithb    .
-cp    $xdef    .
+cp    $vimrc      .
+cp    $bashrc     .
+cp -R $ith        .
+cp -R $ithb       .
+cp    $xdef       .
+cp -R $vimplugins .
